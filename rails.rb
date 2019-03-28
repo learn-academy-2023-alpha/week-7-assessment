2bad8ed3a7258f527ca330c73fc4678e92c41512
Jezreel <jezreel.go@gmail.com>
2019-03-28 11:02:08 -0700

    initial commit

---
+++ rails.rb
@@ -0,0 +1,31 @@
+# We've done the following exercises in Javascript, Now try them in RUBY (You can do them in Javascript again if you want extra practice - not required.)
+#
+# The World Translator
+# Write a Method named hello_world that:
+#
+# takes 1 argument, a language code (e.g. "es", "de", "en")
+# returns "Hello, World" for the given language, for at least 3 languages. It should default to returning English.
+#hello_world("es") should return "Hola Mundo"
+#hello_world("de") should return "Hallo Welt"
+
+
+
+# The Grade Assigner
+# Write a Method named assign_grade that:
+#
+# takes 1 argument, a number score.
+# returns a grade for the score, either "A", "B", "C", "D", or "F".
+# assign_grade(90) should return A
+# assign_grade(75) should return C
+
+
+# The Pluralizer
+# Write a method named pluralizer that:
+#
+# takes 2 arguments, a noun and a number.
+# returns the number and pluralized form of the noun, if necessary.
+# pluralizer(5, cat) should return "5 cats"
+# pluralizer(1, dog) should return "1 dog" because the number one does not require a plural noun
+# Arguments will always be a number and a singular noun.
+
+# Bonus: Make it handle a few collective nouns like "sheep", "geese", "children", "people" and "species".
