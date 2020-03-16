bc83c027b78c6687798b87f0500dac7018d010a2
sjproctor <sproctor950@gmail.com>
2020-03-15 17:39:42 -0700

    finished week 6 build out

--- rails-commenting.rb
+++ rails-commenting.rb
@@ -5,24 +5,24 @@
 
 # app/controller/blog_posts_controller.rb
 
-# 1)
+# 1) A controller class for our blog post model
 class BlogPostsController < ApplicationController
   def index
-    # 2)
+    # 2) creating an instance variable that contains all the entries in the database
     @posts = BlogPost.all
   end
 
   def show
-    # 3)
+    # 3) creating an instance variable that holds one entry from the database, based on an id
     @post = BlogPost.find(params[:id])
   end
 
-  # 4)
+  # 4) a method that will display a form for the user to create a new blog post
   def new
   end
 
   def create
-    # 5)
+    # 5) creating a instance variable that will create a new blog post based on the params stated in the params method
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to @post
@@ -36,15 +36,15 @@ class BlogPostsController < ApplicationController
     if @post.destroy
       redirect_to blog_posts_path
     else
-      # 6)
+      # 6) if the blog post is deleted show the post that was just removed
       redirect_to blog_post_path(@post)
     end
   end
 
-  # 7)
+  # 7) prevents methods below this line from being called outside this class
   private
   def blog_post_params
-    # 8)
+    # 8) defines the attributes that can be given to the new blog_post entry on the form
     params.require(:blog_post).permit(:title, :content)
   end
 
@@ -53,8 +53,8 @@ end
 
 # app/models/blog_post.rb
 
-# 9)
+# 9) the model class of BlogPost created by the generate model/resourse command
 class BlogPost < ApplicationRecord
-  # 10)
+  # 10) the class BlogPost is the parent class of the model class Comment
   has_many :comments
 end
